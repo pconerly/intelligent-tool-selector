3a45390a5c4d3e82cd5909055bbf90bf5190c387
Peter Conerly <pconerly@gmail.com>
2023-08-16 12:24:09 -0700

    Add CORS, clean up backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,16 +2,30 @@ import json
 from os import path
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from sqlalchemy.orm import Session
-# from sqlalchemy.ext.declarative import declarative_base
+
 from models import SessionLocal, ToolDB
 from routes import router as tool_router
-# from tool_fixtures import tool_fixtures
-
-app = FastAPI()
 
 current_directory = path.dirname(path.abspath(__file__))
 
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
